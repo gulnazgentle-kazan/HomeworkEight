f33a329a259384a6f44361c5aff12ae5a1d486ba
gulnazgentle-kazan <gulnazvp@mail.ru>
2023-01-30 22:13:19 +0300

    did and added exercise54

--- Exercise54/Program.cs
+++ Exercise54/Program.cs
@@ -9,56 +9,52 @@
 9 5 3 2
 8 4 4 2*/
 
-
-int[,] table = new int[3, 4];
-FillArrayRandom(table);
-PrintArray(table);
-SortToLower(table);
-Console.WriteLine();
-PrintArray(table);
-
-
-// Функция заполнения массива рандомно числами от 1 до 9
-void FillArrayRandom(int[,] array)
+int[,] GetMatrix(int rows, int colums)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    int[,] matrix = new int[rows, colums];
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(1, 10);
+            matrix[i, j] = new Random().Next(0, 10);
         }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
     }
 }
 
-// Функция сортировки элементов в строке двумерного массива, по убыванию
-void SortToLower(int[,] array)
+void SortMatrixRows(int[,] matrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            for (int k = 0; k < matrix.GetLength(1) - j - 1; k++)
             {
-                if (array[i, k] < array[i, k + 1])
+                if (matrix[i, k] < matrix[i, k + 1])
                 {
-                    int temp = array[i, k + 1];
-                    array[i, k + 1] = array[i, k];
-                    array[i, k] = temp;
+                    int temp = matrix[i, k];
+                    matrix[i, k] = matrix[i, k + 1];
+                    matrix[i, k + 1] = temp;
                 }
             }
         }
     }
 }
+int[,] matrix1 = GetMatrix(3, 4);
+PrintMatrix(matrix1);
+Console.WriteLine("Отсортированный массив ");
+SortMatrixRows(matrix1);
+PrintMatrix(matrix1);
 
-// Функция вывода двумерного массива
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
